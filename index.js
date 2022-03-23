ac259b6c467944fbaf106cc1dfaeaae853fc7bb6
happygoalvin <yo.alvin.96@gmail.com>
2022-03-23 18:12:47 +0800

    managed to get sword_info

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 const MongoUtil = require('./MongoUtil');
 const axios = require('axios');
+const req = require('express/lib/request');
+const { Db } = require('mongodb');
 
 // create the app
 const app = express();
@@ -15,12 +17,50 @@ app.use(express.json());
 // enable CORS
 app.use(cors());
 
-const COLLECTION_NAME1="sword_info"
-const COLLECTION_NAME2="fighting_style"
+// for forms to work
+app.use(express.urlencoded({
+    extended:false
+}));
 
-app.get('/', function(req,res){
-    res.send("It's alive!")
+const COLLECTION_SWORD_INFO="sword_info"
+const COLLECTION_FIGHTING_STYLE="fighting_style"
+
+// ROUTES
+async function main(){
+    await MongoUtil.connect(process.env.MONGO_URI, "all_about_swords")
+
+    app.post('/swords', async (req,res) => {
+        
+        try {
+            let {name, origin, description, image_url, time_period_created, tags, fighting_style} = req.body
+
+        } catch (e) {
+
+        }
+    } 
+)
+
+    app.get('/swords', async (req,res) => {
+    let criteria = {};
+
+    if (req.query.name) {
+        criteria['name'] = {
+            '$regex': req.query.name,
+            '$options':'i'
+        }
+    }
+    
+    const db = MongoUtil.getDB();
+    let sword_info = await db.collection(COLLECTION_SWORD_INFO).find(criteria).toArray();
+    res.json({
+        'sword_info':sword_info
+    })
+    
 })
+
+}
+
+main();
 
 // BEGIN SERVER (aka LISTEN)
 app.listen(3000, function(){
