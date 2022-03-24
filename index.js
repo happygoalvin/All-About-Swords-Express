698e97a4027836f684a3ecaa520e96986a4c9f16
happygoalvin <yo.alvin.96@gmail.com>
2022-03-24 17:42:55 +0800

    Object destructured sword info collection and successfully added post

--- index.js
+++ index.js
@@ -23,18 +23,50 @@ app.use(express.urlencoded({
 
 const COLLECTION_SWORD_INFO="sword_info"
 const COLLECTION_FIGHTING_STYLE="fighting_style"
+const COLLECTION_TAGS = "tags"
 
 // ROUTES
 async function main(){
     await MongoUtil.connect(process.env.MONGO_URI, "all_about_swords")
 
     app.post('/swords', async (req,res) => {
-        
+    
         try {
-            let {name, origin, description, image_url, time_period_created, tags, fighting_style} = req.body
+            let {name, origin, description, image_url, blade, time_period_created} = req.body
+            // console.log(blade.metal)
+            // tags = tags.split(",");
+            // tags = tags.map(function(each_tags){
+            //     return each_tags.trim()
+            // })
 
+            // fighting_style = fighting_style.split(',');
+            // fighting_style = fighting_style.map(function(each_style) {
+            //     return each_style.trim()
+            // })
+
+            const db = MongoUtil.getDB()
+
+
+            await db.collection(COLLECTION_SWORD_INFO).insertOne({
+                name,
+                origin,
+                description,
+                blade,
+                image_url,
+                time_period_created, 
+                tags,
+                fighting_style
+            })
+            res.status(200);
+            res.json({
+                message: "record has been added successfully"
+            })
         } catch (e) {
-
+            res.status(500)
+            res.json({
+                message: "Internal server error. Please contact administrator"
+            })
+            console.log(e)
         }
     } 
 )
@@ -55,6 +87,7 @@ async function main(){
         'sword_info':sword_info
     })
     
+
 })
 
 }
